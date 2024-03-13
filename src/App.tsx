e1806283b9eb16e9fcfa466798d96120e9a8ddc3
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-03-13 03:25:13 -0400

    add Home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 // import { useState } from 'react'
+import { Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
 
 function App() {
-    return <h1>Hi</h1>
+    return <Container>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </Container>
 }
 
 export default App
